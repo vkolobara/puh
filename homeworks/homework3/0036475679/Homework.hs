d2885705b5c897996c78df47cb35bde22c18a9a4
Vinko Kolobara <kolobara.vinko@gmail.com>
2017-10-31 10:13:38 +0100

    Task 1 and task 2 of homework done

--- homeworks/homework3/0036475679/Homework.hs
+++ homeworks/homework3/0036475679/Homework.hs
@@ -7,11 +7,17 @@ import Data.Bits ( xor )
 
 -- Task 01
 localMaxima :: [Int] -> [Int]
-localMaxima = undefined
+localMaxima []         = []
+localMaxima (p:x:s:xs)
+  | x > p && x > s     = x : localMaxima (x:s:xs)
+  | otherwise          = localMaxima (x:s:xs)
+localMaxima (x:xs)     = localMaxima xs
 
 -- Task 02
 transform :: [(Int, String)] -> [(Char, Int)]
-transform = undefined
+transform xs = concat [extractChars val chars | (val, chars) <- xs]
+  where
+    extractChars val chars = [(toLower c, val) | c <- chars]
 
 -- Task 03
 rule90 :: [Bool] -> [[Bool]]
