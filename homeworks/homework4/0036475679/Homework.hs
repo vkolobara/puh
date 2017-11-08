18f8ffd85b1e8486e1cc892450f0d4ebbbf02ac0
Vinko Kolobara <kolobara.vinko@gmail.com>
2017-11-08 22:47:14 +0100

    Homework 4 task 1 done

--- homeworks/homework4/0036475679/Homework.hs
+++ homeworks/homework4/0036475679/Homework.hs
@@ -9,28 +9,28 @@ import Data.Function ( fix )
 
 -- non accumulator style
 factorial :: (Num a, Eq a) => a -> a
-factorial = undefined
+factorial = fix (\rec x -> if x == 0 then 1 else x * rec (x-1))
 
 -- non accumulator style
 sum' :: Num a => [a] -> a
-sum' = undefined
+sum' = fix (\rec xs -> if null xs then 0 else head xs + rec (tail xs))
 
 -- accumulator style
 factorial' :: (Num a, Eq a) => a -> a
-factorial' = undefined
+factorial' = fix (\rec a x -> if x == 0 then a else rec (a*x) (x-1)) 1
 
 -- accumulator style
 sum'' :: Num a => [a] -> a
-sum'' = undefined
+sum'' = fix (\rec a xs -> if null xs then a else rec (head xs + a) (tail xs)) 0
 
 nats :: [Integer]
-nats = undefined
+nats = fix (\rec x -> x:rec (succ x)) 1
 
 map' :: (a -> b) -> [a] -> [b]
-map' = undefined
+map' f = fix (\rec xs -> if null xs then [] else (f $ head xs):rec (tail xs))
 
 zip' :: [a] -> [b] -> [(a, b)]
-zip' = undefined
+zip' = fix (\rec xs ys -> if null xs || null ys then [] else (head xs, head ys):rec (tail xs) (tail ys))
 
 -- Task 02
 subsets :: Int -> [a] -> [[a]]
