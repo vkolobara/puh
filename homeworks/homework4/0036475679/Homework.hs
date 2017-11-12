a2c7dacf7c659259b94b72e489ad68d91b6fbe4b
Vinko Kolobara <kolobara.vinko@gmail.com>
2017-11-12 12:36:13 +0100

    Homework done

--- homeworks/homework4/0036475679/Homework.hs
+++ homeworks/homework4/0036475679/Homework.hs
@@ -41,21 +41,34 @@ subsets n xs
 subsets' :: Eq a => Int -> [a] -> [[a]]
 subsets' 0 _      = [[]]
 subsets' n []     = []
-subsets' n (x:xs) = [x:p | p <- subsets' (n-1) xs]
-                    ++ subsets' n xs
+subsets' n (x:xs) = map (x:) (subsets' (n-1) xs) -- Subsets with the current element
+                    ++ subsets' n xs -- Subsets without the current element
 
-
-partitions :: Eq a => [a] -> [[[a]]]
+partitions :: [a] -> [[[a]]]
 partitions [] = error "partitioning empty set"
-partitions xs = partitions' xs
+partitions xs = partitions'' xs
 
+partitions'' :: [a] -> [[[a]]]
+partitions'' []     = [[]]
+partitions'' (x:xs) = map ([x]:) parts -- insert the element as a separate partition
+                      ++ concatMap (mapWithRemainder x) parts -- insert the element into all partitions
+  where parts = partitions'' xs
+
+-- Maps current element to all the partitions, but retains the remaining partitions, e.g.:
+-- mapWithRemainder 'a' ["bc", "d", ""] -> [["abc", "d", ""], ["bc", "ad", ""], ["bc", "d", "a"]]
+mapWithRemainder :: a -> [[a]] -> [[[a]]]
+mapWithRemainder e xs = [ take i xs ++ (e:xs!!i) : drop (i+1) xs | i <- [0..length xs-1]]
+
+-- Faster implementation with Eq constraint
 partitions' :: Eq a => [a] -> [[[a]]]
 partitions' []     = [[]]
-partitions' (x:xs) = [[x]:ps | ps <- parts] ++ [ (x : p) : delete p ps | ps <- parts, p <- ps]
+partitions' (x:xs) = map ([x]:) parts -- insert the element as a separate partition
+                    ++ [ (x : p) : delete p ps | ps <- parts, p <- ps] -- insert the element into all partitions
   where parts =  partitions' xs
 
-
 -- Task 03
-permutations' :: Eq a => [a] -> [[a]]
-permutations' [] = [[]]
-permutations' xs = [ x:perms | x <- xs, perms <- permutations' $ delete x xs]
+permutations' :: [a] -> [[a]]
+permutations' []     = [[]]
+permutations' [x]    = [[x]]
+permutations' (x:xs) = [ take i perm ++ x : drop i perm | perm <- perms, i <- [0..length perm]] -- insert the omitted element into all positions of the permutations of the remainder
+  where perms = permutations' xs
