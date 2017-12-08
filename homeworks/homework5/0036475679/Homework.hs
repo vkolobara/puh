f444a4c2769cc03c3004f50b6bb14b58732c244b
Vinko Kolobara <kolobara.vinko@gmail.com>
2017-12-08 18:46:47 +0100

    Homework 5 tasks done, exercises left

--- homeworks/homework5/0036475679/Homework.hs
+++ homeworks/homework5/0036475679/Homework.hs
@@ -55,17 +55,17 @@ turnRight WEST  = NORTH
 -- Task 02
 
 data TriangleType = Equilateral | Isosceles | Scalene | Degenerate | Illegal deriving (Eq, Show)
-data Triangel     = Triangle Int Int Int deriving Show
+data Triangle     = Triangle Int Int Int deriving Show
 
 triangleType :: (Ord a, Num a) => a -> a -> a -> TriangleType
 triangleType a b c
-  | any (<=0) [a,b,c]          = Illegal
-  | (not . all isValid) pairs  = Illegal
-  | any isDegenerate pairs     = Degenerate
-  | a == b && b == c           = Equilateral
-  | numSame <= 2               = Isosceles
-  | numSame == 3               = Scalene
-  | otherwise                  = Illegal
+  | any (<=0) [a,b,c]           = Illegal
+  | (not . all isValid) pairs   = Illegal
+  | any isDegenerate pairs      = Degenerate
+  | a == b && b == c            = Equilateral
+  | numSame <= 2                = Isosceles
+  | numSame == 3                = Scalene
+  | otherwise                   = Illegal
   where numSame                 = (length . nub) [a,b,c]
         pairs                   = [((a, b), c), ((a, c), b), ((b, c), a)]
         isDegenerate ((x,y), z) = x+y == z
@@ -85,7 +85,13 @@ triangleType a b c
 
 {- It is MANDATORY to implement 'splitter' function in terms of fold -}
 splitter :: Eq a => [a] -> [a] -> [[a]]
-splitter = undefined
+splitter sublst = foldr f []
+  where len = length sublst
+        -- if we found the sublist on which we should split, then remember the current solution
+        -- and prepend the empty list to it so that we start building another part.
+        -- otherwise prepend everything to the head of the current parts.
+        f x acc = if sublst `isPrefixOf` (x:curr) then []:drop len (x:curr):rest else (x:curr):rest
+          where (curr, rest) = if null acc then ([], []) else (head acc, tail acc)
 
 -- Task 04
 
@@ -94,3 +100,13 @@ splitter = undefined
   solution doesn't exist explain why that is the case. Of corse, solution
   must use fold.
 -}
+
+-- My implementation of splitter has no problems with infinite lists:
+infList :: String
+infList = cycle "123 > 456 > "
+
+takeFromInf :: Int -> [String]
+takeFromInf n = (take n . splitter " > ") infList
+
+first10000 :: [String]
+first10000 = takeFromInf 10000
